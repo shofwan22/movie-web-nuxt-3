82ef49c1b0de21a6f122cbbfb657e1155855cf31
shofwanh <shofwanhanif22@gmail.com>
2023-02-17 01:59:56 +0700

    Add: Implement review on page detail movie

---
+++ components/base/CardReview.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="rounded-[14px] w-[582px] h-[284px] bg-[#F9F9F9] shadow-black/10 py-6 px-5"
+  >
+    <div class="flex items-center justify-between">
+      <div class="flex items-center">
+        <div
+          class="w-[48px] h-[48px] rounded-full mr-[18px] bg-[#1E232B]/[.21]"
+        ></div>
+        <div>
+          <p class="text-sm text-[#1E232A] font-bold mb-[3px]">{{ name }}</p>
+          <p class="text-xs text-[#666666] font-normal">{{ date }}</p>
+        </div>
+      </div>
+      <div
+        class="h-[52px] w-[97px] bg-[#C4C4C4]/[.28] flex items-center justify-center rounded-[7px]"
+      >
+        <img src="~/assets/images/ic_star.png" width="17" height="17" alt="" />
+        <p class="text-black font-semibold text-4xl ml-[6px]">{{ rating }}</p>
+      </div>
+    </div>
+    <div
+      class="mt-[25px] font-normal text-black text-[13px] leading-[150%] italic"
+    >
+      {{ message }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    default: '',
+  },
+  date: {
+    type: String,
+    default: '',
+  },
+  rating: {
+    type: String,
+    default: '',
+  },
+  message: {
+    type: String,
+    default: '',
+  },
+});
+</script>
--- pages/movies/detail/[id].vue
+++ pages/movies/detail/[id].vue
@@ -56,7 +56,21 @@
         </div>
       </div>
     </div>
-    <div class="bg-white h-[1440px] px-20"></div>
+    <div class="bg-white h-full px-20 pt-40 pb-[58px]">
+      <div class="">
+        <p class="uppercase text-sm text-[#FF0000] font-semibold">Reviews</p>
+        <div class="mt-[25px] grid grid-cols-2 gap-x-2">
+          <BaseCardReview
+            v-for="(review, index) in reviews"
+            :key="index"
+            :name="review.name"
+            :date="review.date"
+            :rating="review.rating"
+            :message="review.message"
+          />
+        </div>
+      </div>
+    </div>
     <div class="h-[537px] px-20 py-[50px]">
       <p class="text-base font-semibold text-white uppercase">
         Recommended Movies
@@ -99,6 +113,22 @@ const detailMovie = ref([
   {
     title: 'production',
     info: 'DC Entertainment',
+  },
+]);
+const reviews = ref([
+  {
+    name: 'SWITCH.',
+    date: 'December 18, 2020',
+    rating: '7.0',
+    message:
+      "It isn't as easy as saying 'Wonder Woman 1984' is a good or bad movie. The pieces are there, and there are moments I adore, but it does come across as a bit of a mess, even though the action sequences are breathtaking. If you're a fan of the original film, you'll be more willing to take the ride, but for those more indifferent, it may be a bit of a blander sit. If you can and are planning to watch it, the theatrical experience is the way to go - there is nothing like seeing these stunning sets, fun action scenes and hearing Zimmer's jaw-dropping score like on the big screen. - Chris dos Santos... read the rest.",
+  },
+  {
+    name: 'msbreviews',
+    date: 'December 18, 2020',
+    rating: '8.0',
+    message:
+      "If you enjoy reading my Spoiler-Free reviews, please follow my blog @ https://www.msbreviews.com The superhero genre has been growing exponentially during the last decade, so it's bizarre to go through an entire year with only Birds of Prey and The New Mutants instead of literally dozens of films from both Marvel and DC. Thankfully, Warner Bros. decided to release Wonder Woman 1984 before the year's end, but not without a catch. Most people will only have the possibility of watching one of the few blockbusters of 2020 through HBO Max, a streaming service only",
   },
 ]);
 const listMovie = ref([
